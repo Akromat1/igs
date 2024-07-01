8a6059f6171c3d4f85bc47256589966ddf09fd47
metalhead <114188050+eckeriaue@users.noreply.github.com>
2024-07-02 00:04:09 +0300

    fix code filter

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -4,7 +4,14 @@ import { Codemirror } from 'vue-codemirror';
 
 const socket = new WebSocket("ws://localhost:3000/connect");
 
-const myId = crypto.randomUUID()
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+  );
+}
+
+
+const myId = uuidv4()
 const code = ref('')
 
 socket.addEventListener('message', async event => {
